1248ab3307ba7613d7451d4f55fb0f6438e499f1
Sittuithiang55@gmail.com <sakeisen@pornhub.gov>
2020-08-22 21:53:52 +0530

    End of Section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -48,6 +48,9 @@ namespace DatingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+           
+            
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -78,7 +81,9 @@ namespace DatingApp.API.Controllers
             return Ok(new {
                 token = tokenHandler.WriteToken(token)
             });
+
+           } 
+            
         }
 
     }
-}
